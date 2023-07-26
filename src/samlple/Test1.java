297b9d23f9326d30d127d443090b2e156a9da583
panbarasan <panbarasan176@gmail.com>
2023-07-27 00:42:09 +0530

    this is commit from asian time zone

---
+++ src/samlple/Test1.java
@@ -0,0 +1,25 @@
+package samlple;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void m1() {
+		System.out.println("class Test1 m1");
+	}
+	@Test
+	public void m2() {
+		System.out.println("class Test1 m2");
+	}
+	@Test
+	public void m3() {
+		System.out.println("class Test1 m3");
+	}
+	
+	@Test
+	public void m4() {
+		System.out.println("class Test1 m4");
+	}
+
+}
