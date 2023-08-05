ab2de5950b8ab611d1d22478a97f37aa3ec27771
panbarasan <panbarasan176@gmail.com>
2023-08-05 20:48:14 +0530

    all commit

---
+++ src/samlple4/Test2.java
@@ -0,0 +1,25 @@
+package samlple4;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	@Test
+	public void m1() {
+		System.out.println("class Test1 m1");
+	}
+	@Test
+	public void m2() {
+		System.out.println("class Test1 m2");
+	}
+	@Test
+	public void m3() {
+		System.out.println("class Test1 m3");
+	}
+	
+	@Test
+	public void m4() {
+		System.out.println("class Test1 m4");
+	}
+
+}
